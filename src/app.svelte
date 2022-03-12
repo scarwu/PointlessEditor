36d066b0cc7516de47dc04fcb37635f3f9da5c3a
Scar Wu <xneriscool@gmail.com>
2022-03-12 18:36:58 +0800

    1. update app

--- src/app.svelte
+++ src/app.svelte
@@ -26,7 +26,6 @@ function selectMenu (target) {
         break;
     }
 }
-
 </script>
 
 <style lang="sass">
@@ -35,89 +34,104 @@ $grid-gutter-width: 0
 $fa-font-path: "../../fonts/vendor"
 
 // Import 3rd-Party Libraries
-@import "../node_modules/bootstrap/scss/bootstrap"
 @import "../node_modules/@fortawesome/fontawesome-free/scss/fontawesome"
 @import "../node_modules/@fortawesome/fontawesome-free/scss/solid"
 @import "../node_modules/@fortawesome/fontawesome-free/scss/regular"
 @import "../node_modules/@fortawesome/fontawesome-free/scss/brands"
 
 #pe-editor
-    .pe-label
-        position: fixed
-        top: 0
-        left: 0
-        display: block
-        width: 50px
-        height: 50px
-        text-align: center
-        line-height: 50px
-        cursor: pointer
+    display: flex
 
-    .pe-model
-        position: fixed
-        top: 10%
-        left: 10%
-        right: 10%
-        bottom: 10%
-        border: 1px solid #333
-        border-radius: 5px
-        background-color: #fff
+    position: fixed
+    top: 0
+    left: 0
+    z-index: 9000
+    overflow: hidden
+    width: 5rem
+    height: 5rem
+    background-color: #fff
 
-        .pe-header
+    &.is-show-model
+        right: 0
+        bottom: 0
+        width: inherit
+        height: inherit
 
-        .pe-body
-            .pe-menu
-                background-color: #ddd
+    .pe-left
+        display: flex
+        flex-direction: column
 
-                .pe-option
-                    padding: 0 10px
-                    height: 40px
-                    line-height: 40px
-                    cursor: pointer
+        width: 25rem
 
-            .pe-content
-                background-color: #eee
+        .pe-head
+            .pe-icon
+                display: inline-block
+                width: 5rem
+                height: 5rem
+                text-align: center
+                line-height: 5rem
+                vertical-align: top
+                cursor: pointer
+
+            .pe-title
+                display: inline-block
+                height: 5rem
+                line-height: 5rem
+                font-size: 1.6rem
+                vertical-align: top
+
+        .pe-menu
+            background-color: #ddd
+
+            .pe-option
+                padding: 0 1rem
+                height: 4rem
+                line-height: 4rem
+                cursor: pointer
+
+    .pe-right
+        flex: 1
+
+        .pe-content
+            background-color: #eee
 </style>
 
-<div id="pe-editor">
-    <div class="pe-label" on:click={() => toggleModel()}>
-        <i class="fas fa-user-cog"></i>
-    </div>
-
-    {#if isShowModel}
-    <div class="pe-model">
-        <div class="row">
-            <div class="pe-header col-12">
-
+<div id="pe-editor" class:is-show-model={!isShowModel}>
+    <div class="pe-left">
+        <div class="pe-head">
+            <div class="pe-icon" on:click={() => toggleModel()}>
+                <i class="fas fa-user-cog"></i>
             </div>
-            <div class="pe-body col-12 row">
-                <div class="pe-menu col-3">
-                    <div class="pe-option" on:click={() => selectMenu('article')}>Article</div>
-                    <div class="pe-option" on:click={() => selectMenu('describe')}>Describe</div>
-                    <div class="pe-option" on:click={() => selectMenu('theme')}>Theme</div>
-                    <div class="pe-option" on:click={() => selectMenu('config')}>Config</div>
-                </div>
-                <div class="pe-content col-9">
-                    {#if 'article' === targetMenu}
-                        {#each postList as postItem}
-                        <div>{postItem.title}</div>
-                        {/each}
-                    {/if}
-                    {#if 'describe' === targetMenu}
-                        {#each postList as postItem}
-                        <div>{postItem.title}</div>
-                        {/each}
-                    <div>Theme</div>
-                    {/if}
-                    {#if 'theme' === targetMenu}
-                    <div>Theme</div>
-                    {/if}
-                    {#if 'config' === targetMenu}
-                    <div>Config</div>
-                    {/if}
-                </div>
+            <div class="pe-title">
+                Pointless Editor
             </div>
         </div>
+        <div class="pe-menu">
+            <div class="pe-option" on:click={() => selectMenu('article')}>Article</div>
+            <div class="pe-option" on:click={() => selectMenu('describe')}>Describe</div>
+            <div class="pe-option" on:click={() => selectMenu('theme')}>Theme</div>
+            <div class="pe-option" on:click={() => selectMenu('config')}>Config</div>
+        </div>
     </div>
-    {/if}
+    <div class="pe-right">
+        <div class="pe-content">
+            {#if 'article' === targetMenu}
+                {#each postList as postItem}
+                <div>{postItem.title}</div>
+                {/each}
+            {/if}
+            {#if 'describe' === targetMenu}
+                {#each postList as postItem}
+                <div>{postItem.title}</div>
+                {/each}
+            <div>Theme</div>
+            {/if}
+            {#if 'theme' === targetMenu}
+            <div>Theme</div>
+            {/if}
+            {#if 'config' === targetMenu}
+            <div>Config</div>
+            {/if}
+        </div>
+    </div>
 </div>
